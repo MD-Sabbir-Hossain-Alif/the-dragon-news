229d706c343c06452b61c90494dc121bdcebe624
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2026-04-25 00:13:37 +0600

    project setup done

---
+++ src/app/(main)/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div>
+			<h2 className="text-center text-2xl text-blue-800 underline font-bold">HomePage</h2>
+		</div>
+	);
+}
